23df394070d980e1cd5e390836505424ea8f6d34
rpllpr <rhoadp+github@gmail.com>
2020-04-27 19:05:08 -0500

    basic operations, clear entry, allow for multi-digit numbers

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-function add (a, b) {
+const display = document.querySelector('.display-p');
+let operator = '';
+let operands = [];  
+let operandsIndex = 0;
+
+function addCalc (a, b) {
     return a+b;
 }
 
-function subtract (a, b) {
+function subtractCalc (a, b) {
     return a-b;
 }
 
-function multiply (a, b) {
+function multiplyCalc (a, b) {
     return a*b;
 }
 
-function divide (a, b) {
+function divideCalc (a, b) {
     return a/b;
 }
 
 function operate (operator, a, b) {
-    return operator(a, b);
+    if (operator==='\+') {
+        return addCalc(a,b);
+    } else if (operator==='\-') {
+        return subtractCalc(a,b);
+    } else if (operator==='\*') {
+        return multiplyCalc(a,b);
+    } else {
+        return divideCalc(a,b);
+    }
 }
+
+function evaluateInput(e) {
+    if (e.target.textContent==='\=') {
+        console.log(operator,operands);
+        replaceDisplay(operate(operator, Number(operands[0]), Number(operands[1])));
+    } else if (e.target.textContent==='\+'||e.target.textContent==='\-'||e.target.textContent==='\*'||e.target.textContent==='\/') {
+        operator = e.target.textContent;
+        operandsIndex++;
+        addToDisplay(operator);
+    } else if (e.target.textContent==='CE') {
+        operator = '';
+        operands = [];  
+        operandsIndex = 0;
+        replaceDisplay('');
+        //console.log(operator,operands);
+    } else {
+        let t = operands[operandsIndex];
+        if (operands[operandsIndex]==undefined) {
+            operands[operandsIndex] = e.target.textContent;
+        } else {
+            operands[operandsIndex] = t+e.target.textContent;
+        }
+        // console.log(operands[operandsIndex]);
+        addToDisplay(e.target.textContent);
+    }
+
+}
+
+function addToDisplay (char) {
+    display.textContent += char;
+}
+
+function replaceDisplay (result) {
+    display.textContent = result;
+}
+
+const btns = Array.from(document.querySelectorAll('.button-div'));
+btns.forEach(btn => btn.addEventListener('click', evaluateInput));
